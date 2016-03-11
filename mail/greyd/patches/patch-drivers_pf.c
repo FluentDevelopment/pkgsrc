1c0b5749b8908ed84b754f01b16fa1ccba95b3ff
Timshel Knoll-Miller <timshel@fluentdevelopment.com.au>
2016-03-12 01:25:55 +1100

    Patch to fix build error on pf.c due to invalid extra parameter being passed to signal(3).

---
+++ mail/greyd/patches/patch-drivers_pf.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+--- drivers/pf.c.orig	2015-06-22 05:21:14.000000000 +0000
++++ drivers/pf.c
+@@ -187,7 +187,13 @@ Mod_fw_replace(FW_handle_T handle, const
+         goto err;
+     }
+ 
++#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
++    signal(SIGCHLD, &oldact);
++#else
++    /* This was the original signal(3) call but I don't know what OS supports a 3-argument version
++     * of signal(3)? Leaving it here just in case. */ 
+     signal(SIGCHLD, &oldact, NULL);
++#endif
+ 
+     return nadded;
+ 
