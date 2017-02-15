a59103866e0003d1a831b969ef1dacb6e98bc727
Timshel Knoll-Miller <timshel@fluentdevelopment.com.au>
2017-02-15 11:18:20 +1100

    Add chacha20 option to patch for ChaCha20/Poly1305
    This option includes a patch which has been backported from OpenSSL 1.1
    by CloudFlare. It enables the use of the ChaCha20/Poly1305 cipher which is
    recommended by numerous network security experts including Qualys SSLLabs.

--- security/openssl/options.mk
+++ security/openssl/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD: options.mk,v 1.10 2016/10/03 11:55:11 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openssl
-PKG_SUPPORTED_OPTIONS=	idea md2 mdc2 rc5 zlib threads
+PKG_SUPPORTED_OPTIONS=	chacha20 idea md2 mdc2 rc5 zlib threads
 PKG_SUGGESTED_OPTIONS=	idea md2 mdc2 threads
 
 .include "../../mk/bsd.options.mk"
@@ -37,4 +37,12 @@ CONFIGURE_ARGS+=	no-zlib
 CONFIGURE_ARGS+=	threads
 .else
 CONFIGURE_ARGS+=	no-threads
+.endif
+
+.if !empty(PKG_OPTIONS:Mchacha20) || make(makesum)
+CHACHA20_PATCH=		openssl__chacha20_poly1305_draft_and_rfc_ossl102j.patch
+PATCHFILES+=		${CHACHA20_PATCH}
+SITES.${CHACHA20_PATCH}= https://raw.githubusercontent.com/cloudflare/sslconfig/master/patches/
+PATCH_DIST_STRIP.${CHACHA20_PATCH}= -p1
+PLIST.chacha20=		yes
 .endif
